0a37e1bb769dc16e437bd8c6b31cb6ac9f2d383d
Yaswanth Krishna <yaswanth@dream11.com>
2019-10-28 14:14:50 +0530

    Added Company, User classes Added CustomMap class as a wrapper of google map so that developers are restricted to these function, Added markers for user and company, Added markerContent that need to be shown when user clicks on marker on map

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # maps-typescript
+
 A project on maps where we create random users and companies and show their locations on google map
+
+# pre-requisites
+
+npm install -g parcel-bundler
+
+# Generating Random Data
+
+npm install faker
+
+# Generating API Key for google maps intergration
+
+go to console.developers.google.com
+create a new project
+choose the library Maps Javascript API under the libraries
+create a new API key under credentials
+https://maps.googleapis.com/maps/api/js?key=API_KEY
+Include your API key in place of API_KEY and place this as part of script tag src
+
+# Type Definition Files
+
+@types/faker
+@types/googlemaps
---
+++ src/CustomMap.ts
@@ -0,0 +1,36 @@
+// Instructions on how other classes can use addMarker
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+  constructor(mapDivId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(mapDivId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
